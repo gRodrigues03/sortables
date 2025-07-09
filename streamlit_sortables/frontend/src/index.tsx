200c4f794942133d147669001ef6ccbbc11b979a
rodrigues.cco <bielcreations123@gmail.com>
2025-07-09 16:45:43 -0300

    i don't know what i'm doing part 6

--- streamlit_sortables/frontend/src/index.tsx
+++ streamlit_sortables/frontend/src/index.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SortableComponentWrapper from "./SortableComponent";
 
+
+const observerError = 'ResizeObserver loop completed with undelivered notifications';
+const originalError = window.onerror;
+window.onerror = function(message, source, lineno, colno, error) {
+  if (typeof message === 'string' && message.includes(observerError)) {
+    return true; // Suprime
+  }
+  return originalError?.(message, source, lineno, colno, error);
+};
+
 ReactDOM.render(
     <SortableComponentWrapper />,
     document.getElementById("root")
