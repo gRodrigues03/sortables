26b11943976c23b4d8494bacc72d9696eb0a8626
gRodrigues03 <bielcreations123@gmail.com>
2025-07-09 16:51:04 -0300

    hello from the stupid server

--- sample.py
+++ sample.py
@@ -23,7 +23,7 @@ st.write('Lots of items in a single container.')
 items = [
     {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
 ]
-sorted_items = sort_items(items, multi_containers=True)
+sorted_items = sort_items(items, multi_containers=True, direction='vertical')
 st.write(sorted_items)
 
 st.write('----')
